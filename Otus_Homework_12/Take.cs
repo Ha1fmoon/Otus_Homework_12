683bfcb5269582d77c5f274e02075781cfdedbe4
Nikita Stepanov <nst175@gmail.com>
2025-03-03 08:20:47 +0100

    HW12 Implemented Take method, refactored Top method with overload

---
+++ Otus_Homework_12/Take.cs
@@ -0,0 +1,13 @@
+﻿namespace Otus_Homework_12;
+
+public static partial class Enumerable
+{
+    public static IEnumerable<T> Take<T>(this IEnumerable<T> collection, int count)
+    {
+        ArgumentNullException.ThrowIfNull(collection);
+
+        if (count < 0) throw new ArgumentOutOfRangeException(nameof(count), "Element count must be positive.");
+
+        for (var i = 0; i < count; i++) yield return collection.ElementAt(i);
+    }
+}
--- Otus_Homework_12/Top.cs
+++ Otus_Homework_12/Top.cs
@@ -2,37 +2,36 @@
 
 public static partial class Enumerable
 {
-    public static IEnumerable<TSource> Top<TSource>(this IEnumerable<TSource> source, int percent)
+    public static IEnumerable<TSource> Top<TSource>(this IEnumerable<TSource> source, int percent, bool sort = true)
     {
         CheckSourceAndPercent(source, percent);
 
-        var enumerable = source.OrderByDescending(v => v).ToList();
-        
-        return enumerable.Take(GetPercent(enumerable, percent));
+        var sorted = source;
+
+        if (sort) sorted = source.OrderByDescending(v => v);
+
+        return GetPercent(sorted, percent);
     }
 
-    public static IEnumerable<TSource> Top<TSource, TValue>(this IEnumerable<TSource> source, int percent, Func<TSource, TValue> value)
+    public static IEnumerable<TSource> Top<TSource, TValue>(this IEnumerable<TSource> source, int percent,
+        Func<TSource, TValue> value)
     {
-        CheckSourceAndPercent(source, percent);
-        
-        var enumerable = source.OrderByDescending(value).ToList();
-        
-        return enumerable.Take(GetPercent(enumerable, percent));
+        return source.OrderByDescending(value).Top(percent, false);
     }
-    
+
     private static void CheckSourceAndPercent<TSource>(IEnumerable<TSource>? source, int percent)
     {
         ArgumentNullException.ThrowIfNull(source);
 
-        if (percent > 100 || percent < 1)
-        {
-            throw new ArgumentException("Parameter 'count' must be between 1% and 100%");
-        }
+        if (percent > 100 || percent < 1) throw new ArgumentException("Parameter 'count' must be between 1% and 100%");
     }
 
-    private static int GetPercent<TSource>(this List<TSource>enumerable, int percent)
+    private static IEnumerable<TSource> GetPercent<TSource>(IEnumerable<TSource> sorted, int percent)
     {
-        var percentValue = (float)enumerable.Count * percent / 100;
-        return (int)Math.Ceiling(percentValue);
+        var count = sorted.Count();
+
+        var percentCount = (int)Math.Ceiling((float)count * percent / 100);
+
+        return sorted.Take(percentCount);
     }
 }
