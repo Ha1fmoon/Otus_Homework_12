930c0762c28385b4885425f40f952c9b2b903f20
Nikita Stepanov <nst175@gmail.com>
2025-03-07 16:50:30 +0100

    HW12 Fixed ArgumentOutOfRangeException in Take method, when collection is empty

--- Otus_Homework_12/Take.cs
+++ Otus_Homework_12/Take.cs
@@ -8,6 +8,19 @@ public static partial class Enumerable
 
         if (count < 0) throw new ArgumentOutOfRangeException(nameof(count), "Element count must be positive.");
 
-        for (var i = 0; i < count; i++) yield return collection.ElementAt(i);
+        if (count == 0) yield break;
+
+        var counter = 0;
+
+        foreach (var item in collection)
+            if (counter < count)
+            {
+                yield return item;
+                counter++;
+            }
+            else
+            {
+                yield break;
+            }
     }
 }
